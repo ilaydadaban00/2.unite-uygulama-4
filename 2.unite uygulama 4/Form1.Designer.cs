8bcc2a28df0119878bb4e1d0aeba93bc08d4e13b
İlayda <145451273+ilaydadaban00@users.noreply.github.com>
2024-02-24 14:22:04 +0300

    Add files via upload

---
+++ 2.unite uygulama 4/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace _2.unite_uygulama_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSAyi = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtSAyi
+            // 
+            this.txtSAyi.Location = new System.Drawing.Point(95, 29);
+            this.txtSAyi.Name = "txtSAyi";
+            this.txtSAyi.Size = new System.Drawing.Size(100, 20);
+            this.txtSAyi.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(80, 83);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(144, 83);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "tek mi,çift mi sıfır mı";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(288, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtSAyi);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSAyi;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ 2.unite uygulama 4/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2.unite_uygulama_4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int sayi = Convert.ToInt32(txtSAyi.Text);
+            if(sayi%2 == 0 )
+            {
+                MessageBox.Show("sayı çifttir.");
+            }else if(sayi%2 == 1 )
+            {
+                MessageBox.Show("sayı tekttir.");
+
+            }
+            else
+            {
+                MessageBox.Show("sayı 0 eşittir.");
+            }
+        }
+    }
+}
